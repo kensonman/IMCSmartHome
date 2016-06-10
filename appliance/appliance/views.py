c43a114f7baea7699e263e4d18d297da0230ccd9
Kenson Man <kenson@breakthrough.org.hk>
2016-06-10 12:08:49 +0800

    Update the layout of the dashboard of CCU

--- appliance/appliance/views.py
+++ appliance/appliance/views.py
@@ -19,6 +19,8 @@ def command(req):
 	logger.info('Receive CMD: %s'%cmd)
 	if cmd=='REGISTER':
 		register(req)
+	elif cmd=='ADJUST':
+		power(req)
 	rep=HttpResponse('OK')
 	rep.status_code=200
 	return rep
@@ -41,3 +43,17 @@ def register(req):
 	data['TIME']=tz.now().strftime(FMT)
 	data['PARAMS']=json.dumps({'CMD':'ADJUST', 'AID':cmd[1], 'PARAMS':[{'NAME':'POWER','TYPE':'Boolean','REQUIRED':True},]})
 	requests.post('http://%s:%s/cmd/'%(data['TIP'], data['TPORT']), data=data)
+
+def power(req):
+	'''
+	Turn on/off the light
+	'''
+	cmd=json.loads(req.POST['PARAMS'])
+	if 'POWER' in cmd:
+		pin_light=5
+		val=cmd['POWER'].upper()
+		gpio.setmode(gpio.BOARD)
+		gpio.setup(pin_light, gpio.OUT)
+		gpio.output(pin_light, gpio.HIGHT if val in ['ON', 'YES', '1'] else gpio.LOW)
+	else:
+		print('Unknow power status: %s'%cmd)
